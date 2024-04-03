2e883febc9cc6f69db521ffd369b0cd7c8459394
phongnh <nhphong1804@gmail.com>
2024-04-03 23:41:39 +0700

    Update docker file and docker-compose

--- PBL7/entrypoint.sh
+++ PBL7/entrypoint.sh
@@ -3,8 +3,4 @@
 echo "Apply database migrations"
 python manage.py migrate
 
-echo "starting celery monitor"
-celery -A PBL7 flower --port=5000
-# celery --broker=redis://redis@localhost:6379/0 flower
-
 exec "$@"
