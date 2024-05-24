0cc664a192094c66a42a57c8515beb17ca8dd02b
Vo Huu Nam Truong <namtruong0900@gmail.com>
2024-05-24 07:41:51 +0700

    + save and load model locally

--- API/model_keyphrases/global_var.py
+++ API/model_keyphrases/global_var.py
@@ -1,3 +1,4 @@
+import os
 import transformers
 
 global MAX_LEN 
@@ -10,8 +11,21 @@ MAX_LEN = 256
 global EMBEDDING_MODEL_NAME
 EMBEDDING_MODEL_NAME="bert-base-uncased"
 
+# Define the local path for saving the tokenizer
+local_tokenizer_path = "./model_keyphrases/pretrained_model/bert-base-uncased-tokenizer"
+
 global TOKENIZER
-TOKENIZER = transformers.BertTokenizer.from_pretrained(
-    EMBEDDING_MODEL_NAME,
-    do_lower_case=True
-)
+# TOKENIZER = transformers.BertTokenizer.from_pretrained(
+#     EMBEDDING_MODEL_NAME,
+#     do_lower_case=True
+# )
+
+# Check if the tokenizer is saved locally
+if os.path.exists(local_tokenizer_path):
+    # Load the tokenizer from the local path
+    TOKENIZER = transformers.BertTokenizer.from_pretrained(local_tokenizer_path, do_lower_case=True)
+else:
+    # Load the tokenizer from the Hugging Face Hub
+    TOKENIZER = transformers.BertTokenizer.from_pretrained(EMBEDDING_MODEL_NAME, do_lower_case=True)
+    # Save the tokenizer locally
+    TOKENIZER.save_pretrained(local_tokenizer_path)
