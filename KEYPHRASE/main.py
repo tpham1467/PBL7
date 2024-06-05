391df0adc8e73dedb4a83cbc54550b6a1d9976eb
phong-nh <phong-nh@mcsolutions.vn>
2024-06-05 20:54:27 +0700

    Split docker model

---
+++ KEYPHRASE/main.py
@@ -0,0 +1,18 @@
+# lib
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+]
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
